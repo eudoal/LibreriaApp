c117ae9a3e85bf91167d7c80c44218795c99f83b
eudoal <halleck.g.florida@gmail.com>
2020-02-18 21:14:20 +0100

    ahora si funcionaaaa!!!

--- screens/Añadir.js
+++ screens/Añadir.js
@@ -1,36 +1,14 @@
 import React, {Component} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  Button,
-  StatusBar,
-  Alert,
-  FlatList,
-  TextInput,
-  TouchableOpacity,
-
-  
-  
-} from 'react-native';
-import { exportDefaultSpecifier, tsImportEqualsDeclaration } from '@babel/types';
-
+import { View } from 'react-native';
 import 'react-native-gesture-handler';
 import ModificarAñadir from '../components/ModificarAñadirComponent';
-
 import ActionBarImage from '../components/actionBarImage';
-
 export default class Añadir extends Component{
 
   static navigationOptions = {
     title: 'Añade aquí tu Libro',
       headerRight: () => <ActionBarImage />,
-//      headerStyle: {
-//        backgroundColor: '#e3e3e3',
-//      },
-//      headerTintColor: '#606070',
+
   };
 
   //creamos una función que nos permita la navegación a la pantalla de Inici
@@ -42,7 +20,7 @@ export default class Añadir extends Component{
       render(){
         return(
           <View>
-            <ModificarAñadir text={"Añadir un Libro"} producto={""} inici={this.moveraInici}/>
+            <ModificarAñadir  producto={""} inici={this.moveraInici}/>
           </View>
     
         )
